f3929f180fa0f61331b319fb667d43ba753bdc10
kartik khandelwal <khandelwal.kartik4@gmail.com>
2016-08-07 22:14:15 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
+  database: 'Your Database Name ',
   port: 3000,
-  secretKey: "Arash@$@!#@"
+  secretKey: "Your secret key"
 }
